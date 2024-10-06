cde516c0ea94c380e6b8901ce47e2fb058e84db9
end3r-man <praveen.venkatachalam@proton.me>
2024-10-07 00:37:16 +0530

    first commit

---
+++ lua/plugins/laravel.lua
@@ -0,0 +1,63 @@
+return {
+  {
+    -- Set Laravel Pint as the default PHP formatter with PHP CS Fixer as a fall back.
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        php = { { "pint", "php_cs_fixer" } },
+      },
+    },
+  },
+  {
+    -- Remove phpcs linter.
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        php = {},
+      },
+    },
+  },
+
+  -- Blade syntax highlighting
+  { 'jwalton512/vim-blade' },
+  -- {
+  --   -- Add neotest-pest plugin for running PHP tests.
+  --   -- A package is also available for PHPUnit if needed.
+  --   "nvim-neotest/neotest",
+  --   dependencies = { "V13Axel/neotest-pest" },
+  --   opts = { adapters = { "neotest-pest" } },
+  -- },
+  {
+    -- Add the Laravel.nvim plugin which gives the ability to run Artisan commands
+    -- from Neovim.
+    "adalessa/laravel.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "tpope/vim-dotenv",
+      "MunifTanjim/nui.nvim",
+    },
+    cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
+    keys = {
+      { "<leader>la", ":Laravel artisan<cr>" },
+      { "<leader>lr", ":Laravel routes<cr>" },
+      { "<leader>lm", ":Laravel related<cr>" },
+    },
+    event = { "VeryLazy" },
+    config = true,
+    opts = {
+      lsp_server = "intelephense",
+      features = { null_ls = { enable = false } },
+    },
+  },
+  {
+    -- Add the blade-nav.nvim plugin which provides Goto File capabilities
+    -- for Blade files.
+    "ricardoramirezr/blade-nav.nvim",
+    dependencies = {
+      "hrsh7th/nvim-cmp",
+    },
+    ft = { "blade", "php" }, -- optional, improves startup time
+  }
+}
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,61 @@
+return {
+	-- Highlight, edit, and navigate code
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	main = "nvim-treesitter.configs",
+
+	-- [[ Configure Treesitter ]]
+	opts = {
+		ensure_installed = {
+			"bash",
+			"html",
+			"javascript",
+			"json",
+			"lua",
+			"markdown",
+			"markdown_inline",
+			"python",
+			"query",
+			"regex",
+			"tsx",
+			"typescript",
+			"vim",
+			"yaml",
+			"php",
+		},
+		auto_install = true,
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = { "ruby" },
+		},
+		indent = { enable = true, disable = { "ruby" } },
+	},
+
+	config = function(_, opts)
+		-- Extend the ensure_installed list
+		vim.list_extend(opts.ensure_installed, {
+			"blade",
+		})
+
+		-- Add filetype patterns
+		vim.filetype.add({
+			pattern = {
+				[".*%.blade%.php"] = "blade",
+			},
+		})
+
+		-- Setup nvim-treesitter with the options
+		require("nvim-treesitter.configs").setup(opts)
+
+		-- Configure the parser for blade
+		local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+		parser_config.blade = {
+			install_info = {
+				url = "https://github.com/EmranMR/tree-sitter-blade",
+				files = { "src/parser.c" },
+				branch = "main",
+			},
+			filetype = "blade",
+		}
+	end,
+}
